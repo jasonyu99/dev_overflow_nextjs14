402182f59af29ce43b98ddb6cc5f209b3367a263
jasonyu99 <yujunven@hotmail.com>
2024-03-08 17:35:33 +0800

    tags page ui

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -2,13 +2,14 @@
 
 import User from '@/database/user.model';
 import { connectToDatabase } from '../mongoose';
-import { GetTopInteractedTagsParams } from './shared.types';
+import { GetAllTagsParams, GetTopInteractedTagsParams } from './shared.types';
+import Tag from '@/database/tag.model';
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
     connectToDatabase();
 
-    const { userId, limit = 3 } = params;
+    const { userId } = params;
 
     const user = await User.findById(userId);
 
@@ -27,5 +28,18 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   } catch (error) {
     console.log(error);
     throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return { tags };
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error getting all tags');
   }
 }
